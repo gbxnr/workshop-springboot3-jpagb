2adcafa2ecc418067fbddf1334a2ee20a0037af7
Gabriel Nascimento <gbxnr25@gmail.com>
2025-10-26 20:45:40 -0300

    product e OrderItem

--- src/main/java/com/example/demo/entities/OrderItem.java
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -47,9 +47,11 @@ public class OrderItem implements Serializable {
         id.setOrder(order);
     }
 
+
     public Product getProduct(){
         return id.getProduct();
     }
+
 
     public void setProduct(Product product){
         id.setProduct(product);
