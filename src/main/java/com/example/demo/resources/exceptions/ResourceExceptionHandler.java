eef73a97c12df3aec0a234f899fa04d0f5f02cf8
gbxnr <gbxnr25@gmail.com>
2025-10-27 13:15:19 -0300

    debugar

---
+++ src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.demo.resources.exceptions;
+
+import com.example.demo.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice // vai interceptar as exções
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public <HttpServledRequest> ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServledRequest request){
+
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
