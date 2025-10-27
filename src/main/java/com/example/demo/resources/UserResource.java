6bafeb9414a0c8ef8871ddceff6200f06fa5ae52
gbxnr <gbxnr25@gmail.com>
2025-10-27 12:44:52 -0300

    User update

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -45,6 +45,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 
 	}
+ // Atualização de um usuario
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok(). body(obj);
+	}
 }
 
 
